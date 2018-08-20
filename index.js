e9fbff3e67e5c6d9f0191fef4c3ee97d8dcda70e
Paul Ishiyama <paul.h.ishiyama@gmail.com>
2018-08-20 04:34:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+
+function theBeatlesPlay(artist, instrument) {
+  var Beatles = [];
+  for (var i = 0; i < artist.length; i++) {
+    Beatles.push(artist[i] + " plays " + instrument[i]);
+  }
+  return Beatles;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  var exc = [];
+  while (i < facts.length) {
+    exc.push(facts[i] + "!!!");
+  }
+  
+  return exc;
+}
